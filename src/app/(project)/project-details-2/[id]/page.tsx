eb2bbe7e06a318a14abb6d1693117902d0de93c1
boy397 <aryamukherjee397@outlook.com>
2026-04-24 19:25:37 +0530

    feat: implement project portfolio pages

---
+++ src/app/(project)/project-details-2/[id]/page.tsx
@@ -0,0 +1,28 @@
+
+import ProjectDetailsTwoMain from '@/components/project/ProjectDetailsTwoMain';
+import { PageParamsProps } from '@/types/custom-dt';
+import projectData from '@/data/projectData';
+
+export async function generateStaticParams() {
+    return projectData.map((project) => ({
+        id: project.id.toString(),
+    }));
+}
+
+export async function generateMetadata(props: PageParamsProps) {
+    const resolvedParams = await props.params;
+    const { id } = resolvedParams;
+    const project = projectData.find((item) => item.id == Number(id));
+    return {
+        title: project?.title ? project.title : "Project Details 2 - Digital Agency & Creative Portfolio",
+    };
+}
+
+export default async function ProjectDetails(props: PageParamsProps) {
+    const resolvedParams = await props.params;
+    const { id } = resolvedParams;
+
+    return (
+        <ProjectDetailsTwoMain id={id} />
+    );
+}
---
+++ src/app/(project)/project-details/[id]/page.tsx
@@ -0,0 +1,28 @@
+
+import ProjectDetailsMain from '@/components/project/ProjectDetailsMain';
+import { PageParamsProps } from '@/types/custom-dt';
+import projectData from '@/data/projectData';
+
+export async function generateStaticParams() {
+    return projectData.map((project) => ({
+        id: project.id.toString(),
+    }));
+}
+
+export async function generateMetadata(props: PageParamsProps) {
+    const resolvedParams = await props.params;
+    const { id } = resolvedParams;
+    const project = projectData.find((item) => item.id == Number(id));
+    return {
+        title: project?.title ? project.title : "Project Details - Digital Agency & Creative Portfolio",
+    };
+}
+
+export default async function ProjectDetails(props: PageParamsProps) {
+    const resolvedParams = await props.params;
+    const { id } = resolvedParams;
+
+    return (
+        <ProjectDetailsMain id={id} />
+    );
+}
