411a333c729bc481b2ff4bb8f55c9eb70bc0d16a
boy397 <aryamukherjee397@outlook.com>
2026-04-24 19:25:39 +0530

    feat: implement service listing and detail pages

---
+++ src/app/(services)/service-details/[id]/page.tsx
@@ -0,0 +1,27 @@
+import ServiceDetailsMain from '@/components/service/ServiceDetailsMain';
+import { PageParamsProps } from '@/types/custom-dt';
+import servicesData from "@/data/serviceData";
+
+export async function generateStaticParams() {
+    return servicesData.map((service) => ({
+        id: service.id.toString(),
+    }));
+}
+
+export async function generateMetadata(props: PageParamsProps) {
+    const resolvedParams = await props.params;
+    const { id } = resolvedParams;
+    const service = servicesData.find((item) => item.id == Number(id));
+    return {
+        title: service?.title ? service.title : "Service Details",
+    };
+}
+
+export default async function ServiceDetails(props: PageParamsProps) {
+    const resolvedParams = await props.params;
+    const { id } = resolvedParams;
+
+    return (
+        <ServiceDetailsMain id={id} />
+    );
+}
