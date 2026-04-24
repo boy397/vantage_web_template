7b7b57e1cb97686e716e8ba457f098cf9e4bdbc7
boy397 <aryamukherjee397@outlook.com>
2026-04-24 19:25:17 +0530

    feat: implement blog archive and detail pages

---
+++ src/app/(blogs)/blog-details-2/[id]/page.tsx
@@ -0,0 +1,29 @@
+import BlogDetailsTwoMain from '@/components/blog/BlogDetailsTwoMain';
+import { PageParamsProps } from '@/types/custom-dt';
+import blogData from '@/data/blogData';
+
+export async function generateStaticParams() {
+    return blogData.map((blog) => ({
+        id: blog.id.toString(),
+    }));
+}
+
+export async function generateMetadata(props: PageParamsProps) {
+    const resolvedParams = await props.params;
+    const { id } = resolvedParams;
+    const blog = blogData.find((item) => item.id == Number(id));
+    return {
+        title: blog?.title ? blog.title : "Blog Details 2",
+    };
+}
+
+export default async function BlogDetails(props: PageParamsProps) {
+    const resolvedParams = await props.params;
+    const { id } = resolvedParams;
+
+    return (
+        <main>
+            <BlogDetailsTwoMain id={id} />
+        </main>
+    );
+}
